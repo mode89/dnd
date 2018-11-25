da562e92565995e968c9f5c029bd06336505f311
Mode <mode89@mail.ru>
2018-11-26 00:20:21 +0800

    Use bootstrap

--- __main__.py
+++ __main__.py
@@ -1,11 +1,14 @@
 from flask import Flask
+from flask import render_template
+from flask_bootstrap import Bootstrap
 
-def hello_world():
-    return "Hello, World!"
+def index():
+    return render_template("index.html")
 
 if __name__ == "__main__":
     app = Flask(__name__)
+    Bootstrap(app)
 
-    app.add_url_rule(rule="/", view_func=hello_world)
+    app.add_url_rule(rule="/", view_func=index)
 
     app.run()
