19379d97d1a75ad9f2fc12b30eacbadcaa89a776
Mode <mode89@mail.ru>
2018-11-27 01:14:10 +0800

    Use Bootstrap 4

--- __main__.py
+++ __main__.py
@@ -1,14 +1,10 @@
 from flask import Flask
 from flask import render_template
-from flask_bootstrap import Bootstrap
 
 def index():
     return render_template("index.html")
 
 if __name__ == "__main__":
     app = Flask(__name__)
-    Bootstrap(app)
-
     app.add_url_rule(rule="/", view_func=index)
-
     app.run(host="0.0.0.0")
